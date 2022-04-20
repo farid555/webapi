14346aaa29dc08930ce86ed91d4e2b6bdc6e4928
farid555 <farid555a@gmail.com>
2022-04-21 00:34:03 +0300

    Update request

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -58,5 +58,25 @@ namespace Catalog.Controllers
             repository.CreateItem(item);
             return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
         }
+
+        //PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpddateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+            };
+
+            repository.UpdateItem(updatedItem);
+            return NoContent();
+        }
     }
 }
--- Catalog/Repositories/IItemsRepository.cs
+++ Catalog/Repositories/IItemsRepository.cs
@@ -11,5 +11,7 @@ namespace Catalog.Repositories
         IEnumerable<Item> GetItems();
 
         void CreateItem(Item item);
+
+        void UpdateItem(Item item);
     }
 }
